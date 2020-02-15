92dd15f70446ce949ff35f5bcb3728233f2f8970
Josh Bellmyer <joshbellmyer@gmail.com>
2020-02-15 12:19:25 -0600

    Fixed Enemy Bugs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+	private Rigidbody2D rb;
     private CapsuleCollider2D enemyCollider;
     private Vector2 velocity;
+
     public bool grounded;
     public float moveSpeed;
     public bool moveRight;
@@ -13,7 +15,9 @@ public class Enemy : MonoBehaviour {
     public bool colliding;
 
     void Start() {
+    	rb = GetComponent<Rigidbody2D>();
         enemyCollider = GetComponent<CapsuleCollider2D>();
+
         enemyRadius = enemyCollider.size.x / 2F;
     }
 
@@ -22,21 +26,23 @@ public class Enemy : MonoBehaviour {
     {
         CheckCollisions();
 
-        if (grounded && !colliding)
+        if (grounded)
         {
             velocity.x = moveRight ? moveSpeed : -1 * Mathf.Abs(moveSpeed);
-            transform.Translate(velocity * Time.deltaTime);
+            // transform.Translate(velocity * Time.deltaTime);
+
+            rb.velocity = new Vector2(velocity.x, rb.velocity.y);
         }
 
-
+        transform.eulerAngles = new Vector3(0, 0, 0);
     }
-
 
     void CheckCollisions() {
         // Checks for floor with ledges
         colliding = false;
-        Collider2D[] floorHitsLeft = Physics2D.OverlapCircleAll(new Vector2(transform.position.x - enemyRadius, transform.position.y - 1.5f), 0.01f);
-        Collider2D[] floorHitsRight = Physics2D.OverlapCircleAll(new Vector2(transform.position.x + enemyRadius, transform.position.y - 1.5f), 0.01f);
+        Collider2D[] floorHitsLeft = Physics2D.OverlapCircleAll(new Vector2(transform.position.x - enemyRadius - 0.2f, transform.position.y - 1.5f), 0.01f);
+        Collider2D[] floorHitsRight = Physics2D.OverlapCircleAll(new Vector2(transform.position.x + enemyRadius + 0.2f, transform.position.y - 1.5f), 0.01f);
+
         float horizontalCheckDistance = moveRight ? enemyRadius : -enemyRadius;
         Collider2D[] sideHits = Physics2D.OverlapCircleAll(new Vector2(transform.position.x + horizontalCheckDistance, transform.position.y), 0.01f);
         
@@ -44,20 +50,26 @@ public class Enemy : MonoBehaviour {
             grounded = false;
         else
             grounded = true;
-        
-        if (floorHitsLeft.Length == 0 || floorHitsRight.Length == 0 || sideHits.Length != 0)
-        {
-            if (!sideHits.Any(hit => (hit.gameObject.tag == "Player" || hit == enemyCollider)))
-            {
-               if(grounded)
-                {
-                    Debug.Log($"Floor Hit Left {floorHitsLeft.Length}, Floor Hit Right {floorHitsRight.Length}, Sides {sideHits.Length}");
-                    colliding = true;
-                    moveRight = !moveRight;
-                }
-            }
 
+        if (floorHitsLeft.Length == 0 && grounded) {
+        	moveRight = true;
+
+        	return;
         }
 
+        if (floorHitsRight.Length == 0 && grounded) {
+        	moveRight = false;
+
+        	return;
+        }
+
+        foreach (Collider2D collider2D in sideHits) {
+        	if (collider2D.tag != "Player" && collider2D != enemyCollider) {
+        		colliding = true;
+                moveRight = !moveRight;
+
+                break;
+        	}
+        }
     }
 }
