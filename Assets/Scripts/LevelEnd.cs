d50535c7840c6d494d289a41e83c5a75724745fa
DocQuantum <shchurgood@gmail.com>
2020-02-16 03:14:03 -0600

    Implemented portal animations and made it a prefab

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -6,15 +6,16 @@ using UnityEngine.SceneManagement;
 public class LevelEnd : MonoBehaviour {
 
 	public string nextLevel;
+    private SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start () {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update () {
-        
+        sr.transform.Rotate(0, 0, -100 * Time.deltaTime);
     }
 
     void OnTriggerEnter2D (Collider2D other) {
