3252067f7ced95291402205ff680e0b6bfb5e0a8
Josh Bellmyer <joshbellmyer@gmail.com>
2020-02-16 13:58:01 -0600

    One additional level

--- Assets/Scripts/KillBox.cs
+++ Assets/Scripts/KillBox.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class KillBox : MonoBehaviour
 {
+    public bool dontKillEnemy;
+
     private IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
         string objTag = collision.tag;
         
         if (objTag == "Player")
             collision.gameObject.GetComponent<Player>().KillPlayer();
-        else if (objTag == "Enemy")
+        else if (objTag == "Enemy" && !dontKillEnemy)
             Destroy(collision.gameObject);
 
         yield return new WaitForSeconds(1);
