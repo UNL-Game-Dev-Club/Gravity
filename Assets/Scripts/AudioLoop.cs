917b758e33b43d15fff65b9f9a6d03f17c8cd2ae
DocQuantum <shchurgood@gmail.com>
2020-02-19 01:42:18 -0600

    Really fixed the audio for WebGL

--- Assets/Scripts/AudioLoop.cs
+++ Assets/Scripts/AudioLoop.cs
@@ -12,6 +12,7 @@ public class AudioLoop : MonoBehaviour
     private AudioClip[] clips;
     private double nextEventTime;
     private int flip = 0;
+    private bool looping;
     private AudioSource[] audioSources = new AudioSource[2];
     private bool running = false;
     private void Awake()
@@ -43,12 +44,16 @@ public class AudioLoop : MonoBehaviour
             audioSources[i] = child.AddComponent<AudioSource>();
             audioSources[i].volume = .9f;
         }
-
+#if UNITY_WEBGL
+        nextEventTime = AudioSettings.dspTime + 1.0f;
+#else
         nextEventTime = AudioSettings.dspTime;
+#endif
         running = true;
+        looping = false;
     }
 
-    void Update()
+void Update()
     {
         if (!running)
         {
@@ -57,18 +62,18 @@ public class AudioLoop : MonoBehaviour
 
         double time = AudioSettings.dspTime;
 
-        if (time + 1.0f > nextEventTime && flip < 2)
+        if (time + 1.0f > nextEventTime)
         {
-                
             audioSources[flip].clip = clips[flip];
             audioSources[flip].PlayScheduled(nextEventTime);
 
             nextEventTime += clips[flip].length;
-
-            flip++;
-        }
-        if(flip == 1){
-            audioSources[flip].loop = true;
+            flip = 1 - flip;
+            if(!looping)
+            {
+                clips[0] = clips[1];
+                looping = true;
+            }
         }
     }
 }
--- Assets/Scripts/TitleAudioLoop.cs
+++ Assets/Scripts/TitleAudioLoop.cs
@@ -12,8 +12,9 @@ public class TitleAudioLoop : MonoBehaviour
     private AudioClip[] clips;
     private double nextEventTime;
     private int flip = 0;
+    private bool looping;
     private AudioSource[] audioSources = new AudioSource[2];
-    private bool running = false;
+    public bool running = false;
     private void Awake()
     {
         foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Music"))
@@ -34,7 +35,6 @@ public class TitleAudioLoop : MonoBehaviour
 
     void Start()
     {
-        //gameObject.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
         if (SceneManager.GetActiveScene().name != "Title Screen") Destroy(this.gameObject);
         clips = new AudioClip[] { start, loop };
         for (int i = 0; i < 2; i++)
@@ -45,8 +45,13 @@ public class TitleAudioLoop : MonoBehaviour
             audioSources[i].volume = .9f;
         }
 
+#if UNITY_WEBGL
+        nextEventTime = AudioSettings.dspTime + 1.0f;
+#else
         nextEventTime = AudioSettings.dspTime;
+#endif
         running = true;
+        looping = false;
     }
 
     void Update()
@@ -58,18 +63,18 @@ public class TitleAudioLoop : MonoBehaviour
 
         double time = AudioSettings.dspTime;
 
-        if (time + 1.0f > nextEventTime && flip < 2)
+        if (time + 1.0f > nextEventTime)
         {
-                
             audioSources[flip].clip = clips[flip];
             audioSources[flip].PlayScheduled(nextEventTime);
 
             nextEventTime += clips[flip].length;
-
-            flip++;
-        }
-        if(flip == 1){
-            audioSources[flip].loop = true;
+            flip = 1 - flip;
+            if(!looping)
+            {
+                clips[0] = clips[1];
+                looping = true;
+            }
         }
     }
 }
